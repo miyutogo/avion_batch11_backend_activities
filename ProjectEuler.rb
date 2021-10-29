5c9da8bf896e982e074aa7d1ae6655666bbe2693
Miyu Togo <togomiyu@gmail.com>
2021-10-29 12:51:21 +0800

    Project Euler 1

--- ProjectEuler.rb
+++ ProjectEuler.rb
@@ -1,10 +1,11 @@
 sum  = 0
 
 number = (1...999)
-number.each do |x|
-    if (i%3 == 0 or i%5 == 0):
+number.each do |i|
+    if (i % 3 == 0 || i % 5 == 0)
         sum += i
     end
+end 
+
 puts sum
 
-end
