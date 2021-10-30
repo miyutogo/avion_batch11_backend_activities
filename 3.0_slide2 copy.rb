1e433ced719b38aa9bfbe7eeb5be2705fb7d3b5c
Miyu Togo <togomiyu@gmail.com>
2021-10-30 09:21:59 +0800

    3.0 slide 2 Code Exercise

--- 3.0_slide2 copy.rb
+++ 3.0_slide2 copy.rb
@@ -1,3 +1,8 @@
+#KEEP HYDRATED!
+#Nathan loves cycling.
+#Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+#You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
 def litres(t)
     val = t.div(2)
     puts val
