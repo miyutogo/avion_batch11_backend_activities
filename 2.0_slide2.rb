2444936956e83cefe477c1a7c1fb43770aa58fc8
Miyu Togo <togomiyu@gmail.com>
2021-10-30 09:21:44 +0800

    3.0 slide 2 Code Exercise

---
+++ 2.0_slide2.rb
@@ -0,0 +1,10 @@
+#Find the smallest integer in the array
+
+arr = [34, 15, 88 ,2]
+start = arr[0]
+smallest = 
+
+while start < arr.length - 1 do
+    if start == start
+        smallest = arr
+end
---
+++ 3.0_slide2 copy.rb
@@ -0,0 +1,14 @@
+def litres(t)
+    val = t.div(2)
+    puts val
+end
+
+
+def litres(t)
+    val = (t * 0.5).floor
+    puts val
+end
+
+
+#execute
+litres(3)
